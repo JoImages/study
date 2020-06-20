5daeb2274a376597de9cb731a5592eba1ecfb64b
jason <miss2671228@163.com>
2020-06-20 08:40:53 +0800

    新增： 1.删除倒数第几个节点 2.判断单向链表中是否包含环 3.求链表中间的节点 算法实现

--- src/main/java/com/jason/NodeListDemo.java
+++ src/main/java/com/jason/NodeListDemo.java
@@ -21,9 +21,80 @@ public class NodeListDemo {
         n2.next.next.next = new Node(5);
         n2.next.next.next.next = new Node(6);
 
-        System.out.println(mergeTwoNode(n1, n2));
+        System.out.println(deleteLastKth(n1,3));
 
 
+    }
+
+    /**
+     * 删除倒数第几个节点
+     * @param node
+     * @param lastNum
+     * @return
+     */
+    private static Node deleteLastKth(Node node, Integer lastNum) {
+
+        int length = 0;
+        Node p = node;
+        while (p != null) {
+            length++;
+            p = p.next;
+        }
+        p = node;
+        int number = length - lastNum;
+        if (number < 0) {
+            return p;
+        }
+        if (number == 0) {
+            p = p.next;
+            return p;
+        }
+        int counter = 0;
+        while (p != null) {
+            counter++;
+            if (counter == number) {
+                p.next = p.next.next;
+                return p;
+            }
+            p = p.next;
+        }
+
+        return p;
+    }
+
+    /**
+     * 判断单向链表中是否包含环
+     * @param node
+     * @return
+     */
+    private static boolean existsCircle(Node node) {
+        Node fast = node;
+        Node slow = node;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast.equals(slow)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 求链表中间的节点
+     * @param node
+     * @return
+     */
+    private static Node findMiddleNode(Node node) {
+        Node fast = node;
+        Node slow = node;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
     }
 
     /**
