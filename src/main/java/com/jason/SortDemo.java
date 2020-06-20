b89e7ed4219b09363d415a0331f11db5341d2926
jason <miss2671228@163.com>
2020-06-20 12:01:56 +0800

    新增： 1.排序算法类

---
+++ src/main/java/com/jason/SortDemo.java
@@ -0,0 +1,10 @@
+package com.jason;
+
+/**
+ * 排序算法
+ * @author: 祁琦
+ * @date: 2020/6/20 9:55
+ * version: 1.0
+ */
+public class SortDemo {
+}
--- src/main/java/com/jason/queue/QueueDemo.java
+++ src/main/java/com/jason/queue/QueueDemo.java
@@ -1,4 +1,4 @@
-package com.jason;
+package com.jason.queue;
 
 import java.util.Objects;
 
