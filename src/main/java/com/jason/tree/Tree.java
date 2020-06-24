bf7f9794894c27b80465a67c98fb09f185bd2561
jason <miss2671228@163.com>
2020-06-24 08:43:26 +0800

    新增：       1.二叉查找树的删除操作

--- src/main/java/com/jason/tree/Tree.java
+++ src/main/java/com/jason/tree/Tree.java
@@ -1,7 +1,5 @@
 package com.jason.tree;
 
-import javax.xml.soap.Node;
-
 /**
  * 二叉树学习
  *
@@ -19,13 +17,64 @@ public class Tree {
         node.left.right = new Node(18);
         node.right.left = new Node(34);
         node.right.right = new Node(58);
-        insert(node, 25);
+        delete(node, 13);
         System.out.println(node);
 //        System.out.println(find(node, 33));
     }
 
     /**
+     * 二叉查找树的删除操作
+     *
+     * @param tree  二叉查找树
+     * @param data 需要删除的节点的值
+     */
+
+    private static void delete(Node tree, int data) {
+        // p指向要删除的节点，初始化指向根节点
+        Node p = tree;
+        // pp记录的是p的父节点
+        Node pp = null;
+        while (p != null && p.data != data) {
+            pp = p;
+            if (data > p.data) {
+                p = p.right;
+            } else {
+                p = p.left;
+            }
+        }
+        // 没有找到
+        if (p == null){ return;}
+
+        // 要删除的节点有两个子节点
+        // 查找右子树中最小节点
+        if (p.left != null && p.right != null) {
+            Node minP = p.right;
+            // minPP表示minP的父节点
+            Node minPP = p;
+            while (minP.left != null) {
+                minPP = minP;
+                minP = minP.left;
+            }
+            // 将minP的数据替换到p中
+            p.data = minP.data;
+            // 下面就变成了删除minP了
+            p = minP;
+            pp = minPP;
+        }
+        // 删除节点是叶子节点或者仅有一个子节点
+        Node child; // p的子节点
+        if (p.left != null){ child = p.left;}
+        else if (p.right != null){ child = p.right;}
+        else{ child = null;}
+        // 删除的是根节点
+        if (pp == null){ tree = child;}
+        else if (pp.left == p){ pp.left = child;}
+        else{ pp.right = child;}
+    }
+
+    /**
      * 二叉查找树的插入操作
+     *
      * @param node 希望插入的二叉查找树
      * @param data 希望插入的节点的值
      */
@@ -33,7 +82,7 @@ public class Tree {
         Node p = node;
         Node newNode = new Node(data);
         while (p != null) {
-            if (data < p.val) {
+            if (data < p.data) {
                 if (p.left == null) {
                     p.left = newNode;
                     return;
@@ -61,10 +110,10 @@ public class Tree {
         if (node == null) {
             return null;
         }
-        if (node.val == data) {
+        if (node.data == data) {
             return node;
         }
-        if (data < node.val) {
+        if (data < node.data) {
             node = node.left;
         } else {
             node = node.right;
@@ -81,7 +130,7 @@ public class Tree {
         if (node == null) {
             return;
         }
-        System.out.println(node.val);
+        System.out.println(node.data);
         preOrder(node.left);
         preOrder(node.right);
     }
@@ -96,7 +145,7 @@ public class Tree {
             return;
         }
         inOrder(node.left);
-        System.out.println(node.val);
+        System.out.println(node.data);
         inOrder(node.right);
     }
 
@@ -111,25 +160,25 @@ public class Tree {
         }
         postOrder(node.left);
         postOrder(node.right);
-        System.out.println(node.val);
+        System.out.println(node.data);
     }
 
     private static class Node {
-        int val;
+        int data;
         Node left;
         Node right;
 
         public Node() {
         }
 
-        public Node(int val) {
-            this.val = val;
+        public Node(int data) {
+            this.data = data;
         }
 
         @Override
         public String toString() {
             return "Node{" +
-                    "val=" + val +
+                    "data=" + data +
                     ", left=" + left +
                     ", right=" + right +
                     '}';
